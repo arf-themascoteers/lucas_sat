141862a95fe384601119e0064c280f9c982a2f61
Arif <arif@themascoteers.com>
2024-03-02 19:51:34 +1100

    done

---
+++ ann_simple.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from ann_base import ANNBase
+
+
+class ANNSimple(ANNBase):
+    def __init__(self, train_ds, test_ds, validation_ds):
+        super().__init__(train_ds, test_ds, validation_ds)
+        self.linear = nn.Sequential(
+            nn.Linear(train_ds.x.shape[1],20),
+            nn.LeakyReLU(),
+            nn.Linear(20, 10),
+            nn.LeakyReLU(),
+            nn.Linear(10,1)
+        )
+
+    def forward(self,x):
+        return self.linear(x)
+
--- test_mlp_elem.py
+++ test_mlp_elem.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -21,7 +21,7 @@ train_x = train_data[:,0:-1]
 train_y = train_data[:,-1]
 test_x = test_data[:,0:-1]
 test_y = test_data[:,-1]
-model_instance = LinearRegression()
-model_instance = model_instance.fit(train_x, train_y)
-r2 = model_instance.score(test_x, test_y)
+mlp_regressor = MLPRegressor(hidden_layer_sizes=(50,), max_iter=1000, random_state=42)
+mlp_regressor = mlp_regressor.fit(train_x, train_y)
+r2 = mlp_regressor.score(test_x, test_y)
 print(r2)
